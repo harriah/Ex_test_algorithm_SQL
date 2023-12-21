6a8ad05d80f4fe52f322b95d0b5fe1edaa8f879e
Vũ Thanh Hải <102423481+harriah@users.noreply.github.com>
2023-12-21 14:47:16 +0700

    Add files via upload

---
+++ Baii8FE2/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Baii8FE2
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.labelDN = new System.Windows.Forms.Label();
+            this.labelMK = new System.Windows.Forms.Label();
+            this.txtDangNhap = new System.Windows.Forms.TextBox();
+            this.txtMatkhau = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(389, 233);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(100, 23);
+            this.btnDangNhap.TabIndex = 0;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // labelDN
+            // 
+            this.labelDN.AutoSize = true;
+            this.labelDN.Location = new System.Drawing.Point(285, 147);
+            this.labelDN.Name = "labelDN";
+            this.labelDN.Size = new System.Drawing.Size(98, 16);
+            this.labelDN.TabIndex = 1;
+            this.labelDN.Text = "Tên đăng nhập";
+            // 
+            // labelMK
+            // 
+            this.labelMK.AutoSize = true;
+            this.labelMK.Location = new System.Drawing.Point(285, 181);
+            this.labelMK.Name = "labelMK";
+            this.labelMK.Size = new System.Drawing.Size(61, 16);
+            this.labelMK.TabIndex = 2;
+            this.labelMK.Text = "Mật khẩu";
+            // 
+            // txtDangNhap
+            // 
+            this.txtDangNhap.Location = new System.Drawing.Point(389, 147);
+            this.txtDangNhap.Name = "txtDangNhap";
+            this.txtDangNhap.Size = new System.Drawing.Size(158, 22);
+            this.txtDangNhap.TabIndex = 3;
+            this.txtDangNhap.Text = "Nhập 6-20 kí tự";
+            // 
+            // txtMatkhau
+            // 
+            this.txtMatkhau.Location = new System.Drawing.Point(389, 181);
+            this.txtMatkhau.Name = "txtMatkhau";
+            this.txtMatkhau.PasswordChar = '*';
+            this.txtMatkhau.Size = new System.Drawing.Size(158, 22);
+            this.txtMatkhau.TabIndex = 4;
+            this.txtMatkhau.Text = "Mật khẩu";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(555, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 119);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(555, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 137);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMatkhau);
+            this.Controls.Add(this.txtDangNhap);
+            this.Controls.Add(this.labelMK);
+            this.Controls.Add(this.labelDN);
+            this.Controls.Add(this.btnDangNhap);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Label labelDN;
+        private System.Windows.Forms.Label labelMK;
+        private System.Windows.Forms.TextBox txtDangNhap;
+        private System.Windows.Forms.TextBox txtMatkhau;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Baii8FE2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace Baii8FE2
+{
+    public partial class LoginForm : Form
+    {
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+        private void InitializeLoginForm()
+        {
+            // Khóa nút đăng nhập khi chưa nhập user/pass
+            btnDangNhap.Enabled = false;
+        }
+            private void btnDangNhap_Click(object sender, EventArgs e)
+            {
+                if ((txtDangNhap.Text == "") || (txtMatkhau.Text == ""))
+                {
+                    MessageBox.Show("Vui lòng nhập thông tin", "Thông báo");
+                }
+                else
+                {
+                    if ((txtDangNhap.Text == "thanh") && (txtMatkhau.Text == "123"))
+                    {
+                        MessageBox.Show("Xin chào Thanh", "Thông báo");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Đăng nhập không thành công", "Thông báo");
+                    }
+                }
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtMatkhau.PasswordChar == '*')
+            {
+                button1.BringToFront();
+                txtMatkhau.PasswordChar = '\0';
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtMatkhau.PasswordChar == '\0')
+            {
+                button2.BringToFront();
+                txtMatkhau.PasswordChar = '*';
+            }
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
