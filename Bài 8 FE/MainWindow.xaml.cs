1aa70abe9a55b71827f64ba900eaa1a76453f72a
Vũ Thanh Hải <102423481+harriah@users.noreply.github.com>
2023-12-21 12:14:24 +0700

    Add files via upload

---
+++ Bài 8 FE/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bài_8_FE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            // Khởi tạo form đăng nhập
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
